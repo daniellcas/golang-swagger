bb8ec49e9e14a3f2a2f5800a7272628d3a4940cb
Daniel Santos <danielsantos120615@gmail.com>
2024-05-17 16:10:57 -0300

    iniciando a geracao do swagger

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/daniellcas/golang-swagger/docs"
 	"github.com/daniellcas/golang-swagger/internal/infra/webserver/handlers"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// @title Finances example api
+// @version v0.0.1
+// @description This is an example of an api using swagger
+
+// @contact.name Daniel lucas
+// @contact.email danielsantos120615@gmail.com
+
+// @BasePath /
 func main() {
 	// Creating WebServer
 	ws := http.NewServeMux()
 
 	// Creating routes
 	ws.HandleFunc("/finances", handlers.GetFinances)
+
+	// Docs
+	ws.HandleFunc("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:3000/docs/doc.json")))
 
 	// Starting WebServer
 	http.ListenAndServe(":3000", ws)
